c25cf61fb002d9caf0cc9934617fabea86b1a220
Matrix030 <rishigharat@gmail.com>
2025-08-21 12:20:41 -0400

    handles the registering of new users

---
+++ handler_register.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	db "github.com/Matrix030/gator/internal/database"
+	"github.com/google/uuid"
+	"os"
+	"time"
+)
+
+func handlerRegisterUsers(s *state, cmd command) error {
+
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage: %s <name>", cmd.Name)
+	}
+	name := cmd.Args[0]
+
+	//create a new user in the DB
+	ctx := context.Background()
+	now := time.Now().UTC()
+
+	params := db.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: now,
+		UpdatedAt: now,
+		Name:      name,
+	}
+
+	user, err := s.db.CreateUser(ctx, params)
+	if err != nil {
+		fmt.Println("could not create user:", err)
+		os.Exit(1)
+	}
+
+	if err := s.cfg.SetUser(name); err != nil {
+		return err
+	}
+
+	fmt.Printf("User %q created\n", name)
+	fmt.Printf("debug: %v\n", user)
+	return nil
+}
